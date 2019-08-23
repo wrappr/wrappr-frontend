b243a1e02646ce175cc8f77996f6dae4a0624ce0
Daniel Malik <mail@fronbasal.de>
2019-08-23 14:09:22 +0200

    feat: add sentry

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {getStore} from "./reducers";
 import {Provider} from "react-redux";
+import * as Sentry from '@sentry/browser';
 
 const store = getStore();
+
+Sentry.init({dsn: "https://91a5e593c3ff4e3e896cc14ddb401a2c@sentry.io/1539919"});
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
